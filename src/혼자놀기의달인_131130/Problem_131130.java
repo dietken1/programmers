c72da886a68a2f3d5009900664c7b3d91992f76b
dietken1 <dietken1@ajou.ac.kr>
2026-02-04 19:12:57 +0900

    [131130] 혼자 놀기의 달인

---
+++ src/혼자놀기의달인_131130/Problem_131130.java
@@ -0,0 +1,37 @@
+package 혼자놀기의달인_131130;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Problem_131130 {
+  public static int solution(int[] cards) {
+    ArrayList<Integer> results = new ArrayList<>();
+    for (int i = 1; i <= cards.length; i++) {
+      results.add(sol(cards, i));
+    }
+    List<Integer> topTwo = results.stream()
+        .sorted(Comparator.reverseOrder())
+        .limit(2)
+        .collect(Collectors.toList());
+
+    int result = topTwo.get(0) * topTwo.get(1);
+    System.out.println(result);
+    return result;
+  }
+
+  public static int sol(int[] cards, int start) {
+    int cnt = 0;
+
+    while (true) {
+      int temp = cards[start-1];
+      if (cards[start-1] != 0) {
+        cnt += 1;
+        cards[start-1] = 0;
+        start = temp;
+        continue;
+      }
+      break;
+    }
+    return cnt;
+  }
+}

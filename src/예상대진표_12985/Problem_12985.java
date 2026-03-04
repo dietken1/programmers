c165fe8439ceec886eb67db529b3a45c7cc9e781
dietken1 <dietken1@ajou.ac.kr>
2026-03-04 14:37:50 +0900

    [12985] 예상 대진표

---
+++ src/예상대진표_12985/Problem_12985.java
@@ -0,0 +1,16 @@
+package 예상대진표_12985;
+
+public class Problem_12985 {
+  public int solution(int n, int a, int b)
+  {
+    int answer = 0;
+
+    while (a != b) {
+      a = (int) Math.round(a/2.0);
+      b = (int) Math.round(b/2.0);
+      answer += 1;
+    }
+
+    return answer;
+  }
+}
--- src/혼자놀기의달인_131130/Problem_131130.java
+++ src/혼자놀기의달인_131130/Problem_131130.java
@@ -4,34 +4,34 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 class Problem_131130 {
-  public static int solution(int[] cards) {
-    ArrayList<Integer> results = new ArrayList<>();
-    for (int i = 1; i <= cards.length; i++) {
-      results.add(sol(cards, i));
-    }
-    List<Integer> topTwo = results.stream()
-        .sorted(Comparator.reverseOrder())
-        .limit(2)
-        .collect(Collectors.toList());
-
-    int result = topTwo.get(0) * topTwo.get(1);
-    System.out.println(result);
-    return result;
-  }
-
-  public static int sol(int[] cards, int start) {
-    int cnt = 0;
-
-    while (true) {
-      int temp = cards[start-1];
-      if (cards[start-1] != 0) {
-        cnt += 1;
-        cards[start-1] = 0;
-        start = temp;
-        continue;
+  public int solution(int[] cards) {
+      ArrayList<Integer> results = new ArrayList<>();
+      for (int i = 1; i <= cards.length; i++) {
+        results.add(sol(cards, i));
       }
-      break;
+      List<Integer> topTwo = results.stream()
+          .sorted(Comparator.reverseOrder())
+          .limit(2)
+          .collect(Collectors.toList());
+
+      int result = topTwo.get(0) * topTwo.get(1);
+      return result;
     }
-    return cnt;
+
+    public int sol(int[] cards, int start) {
+      int cnt = 0;
+
+      while (true) {
+        int temp = cards[start-1];
+        if (cards[start-1] != 0) {
+          cnt += 1;
+          cards[start-1] = 0;
+          start = temp;
+          continue;
+        }
+        break;
+      }
+      return cnt;
+    }
   }
 }
